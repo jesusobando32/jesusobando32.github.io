7324af3bc76ebaa8505c29ffff59a4c4adf4cdab
Jesus Obando <jdobando.23@est.ucab.edu.ve>
2026-08-19 14:39:51 -0400

    chore: Update base URL to root for github.io domain

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Configurado para GitHub Pages
+  base: '/', // Configurado para GitHub Pages (root domain)
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
